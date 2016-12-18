df44bb8c65b25abb22fe548ef8848f50e566314f
cctry <csycfl@gmail.com>
2016-12-18 13:58:53 +0800

    complete proprocess

--- utils/rsa.py
+++ utils/rsa.py
@@ -5,6 +5,7 @@ from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
 from Crypto.Signature import PKCS1_v1_5 as Signature_pkcs1_v1_5
 from Crypto.PublicKey import RSA
 import base64
+from utils import mathFunc as mfc
 #global var
 random_generator = Random.new().read
 #functions
@@ -35,11 +36,11 @@ def decrypt(key, msg):
     cipher = Cipher_pkcs1_v1_5.new(rsakey)
     text = cipher.decrypt(base64.b64decode(msg), random_generator)
 
-def preprocess(img):#preprocess the image: convert matrix to array
+def preprocess(img):#preprocess the image: convert matrix to str
     lst = img.tolist()
     temp = list()
     for ele in lst:
         temp += ele
-    res = map(lambda a: int(a), temp)#from lsft to right, from up to down
-    #convert list to str TODO
+    bitarray = map(lambda a: int(a), temp)#from left to right, from up to down
+    res = mfc.bit2str(bitarray)#convert list to str
     return res

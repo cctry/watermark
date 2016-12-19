089fb14611ea0dae7d4fbce6e7ef666273c24aa6
cctry <csycfl@gmail.com>
2016-12-19 22:58:28 +0800

    rewriting rsa as a class

--- utils/rsa.py
+++ utils/rsa.py
@@ -5,42 +5,42 @@ from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
 from Crypto.Signature import PKCS1_v1_5 as Signature_pkcs1_v1_5
 from Crypto.PublicKey import RSA
 import mathFunc as mfc
-#global var
-random_generator = Random.new().read
-#functions
-def generateKey(length):#length is as long as the block size times 8
-    global random_generator
-    rsa = RSA.generate(length, random_generator)#make sure the length after encrypt is qulify
-    private_pem = rsa.exportKey()#
-    with open('private.pem', 'w') as f:
-        f.write(private_pem)
-    public_pem = rsa.publickey().exportKey()
-    with open('public.pem', 'w') as f:
-        f.write(public_pem)
+class RSA:
+    #attributes
+    random_generator = Random.new().read
+    #methods
+    def generateKey(self, length):#length is as long as the block size times 8
+        rsa = RSA.generate(length, self.random_generator)#make sure the length after encrypt is qulify
+        private_pem = rsa.exportKey()#
+        with open('private.pem', 'w') as f:
+            f.write(private_pem)
+        public_pem = rsa.publickey().exportKey()
+        with open('public.pem', 'w') as f:
+            f.write(public_pem)
 
-def loadKey(filename):
-    with open(filename) as f:
-        key = f.read()
-    return key
-    
-def encrypt(key, msg):
-    rsakey = RSA.importKey(key)
-    cipher = Cipher_pkcs1_v1_5.new(rsakey)
-    cipher_msg = cipher.encrypt(msg)
-    return cipher_msg
+    def loadKey(self, filename):
+        with open(filename) as f:
+            key = f.read()
+        return key
+        
+    def encrypt(self, key, img):
+        msg = self.preprocess(img)
+        rsakey = RSA.importKey(key)
+        cipher = Cipher_pkcs1_v1_5.new(rsakey)
+        cipher_msg = cipher.encrypt(img)
+        return cipher_msg
 
-def decrypt(key, msg):
-    global random_generator
-    rsakey = RSA.importKey(key)
-    cipher = Cipher_pkcs1_v1_5.new(rsakey)
-    text = cipher.decrypt(msg, random_generator)
-    return text
+    def decrypt(self, key, msg):
+        rsakey = RSA.importKey(key)
+        cipher = Cipher_pkcs1_v1_5.new(rsakey)
+        text = cipher.decrypt(msg, self.random_generator)
+        return text
 
-def preprocess(img):#preprocess the image: convert matrix to str
-    lst = img.tolist()
-    temp = list()
-    for ele in lst:
-        temp += ele
-    bitarray = map(lambda a: int(a), temp)#from left to right, from up to down
-    res = mfc.bit2str(bitarray)#convert list to str
-    return res
+    def preprocess(self, img):#preprocess the image: convert matrix to str
+        lst = img.tolist()
+        temp = list()
+        for ele in lst:
+            temp += ele
+        bitarray = map(lambda a: int(a), temp)#from left to right, from up to down
+        res = mfc.bit2str(bitarray)#convert list to str
+        return res

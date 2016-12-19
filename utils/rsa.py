571b2982fb37380875f6995d896b20e7f3ae5cfa
cctry <csycfl@gmail.com>
2016-12-19 17:45:49 +0800

    compelete rsa

--- utils/rsa.py
+++ utils/rsa.py
@@ -4,14 +4,13 @@ from Crypto.Hash import SHA
 from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
 from Crypto.Signature import PKCS1_v1_5 as Signature_pkcs1_v1_5
 from Crypto.PublicKey import RSA
-import base64
 import mathFunc as mfc
 #global var
 random_generator = Random.new().read
 #functions
-def generateKey():
+def generateKey(length):#length is as long as the block size times 8
     global random_generator
-    rsa = RSA.generate(1024, random_generator)
+    rsa = RSA.generate(length, random_generator)#make sure the length after encrypt is qulify
     private_pem = rsa.exportKey()#
     with open('private.pem', 'w') as f:
         f.write(private_pem)
@@ -27,14 +26,15 @@ def loadKey(filename):
 def encrypt(key, msg):
     rsakey = RSA.importKey(key)
     cipher = Cipher_pkcs1_v1_5.new(rsakey)
-    cipher_msg = base64.b64encode(cipher.encrypt(msg))
-    return msg
+    cipher_msg = cipher.encrypt(msg)
+    return cipher_msg
 
 def decrypt(key, msg):
     global random_generator
     rsakey = RSA.importKey(key)
     cipher = Cipher_pkcs1_v1_5.new(rsakey)
-    text = cipher.decrypt(base64.b64decode(msg), random_generator)
+    text = cipher.decrypt(msg, random_generator)
+    return text
 
 def preprocess(img):#preprocess the image: convert matrix to str
     lst = img.tolist()

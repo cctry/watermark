c62c2968cf11a58f47b1414ca9598af846324916
ipotato1 <534967057@qq.com>
2016-12-19 17:37:59 +0800

    还没试，晚上回来试。

--- core/preprocess.py
+++ core/preprocess.py
@@ -4,19 +4,14 @@ import cv2
 def preprocess(img, mark, flag):#if colered image, flag == 1  
     if flag == 1:#extract green channel
         green = mfc.getGChn(img)    
-    mark = _markProcess(mark)
+        mark = _markProcess(mark)
     if mark.shape == img.shape:
-        return img, green, mark
-    else:
-        if img.shape[0] >= mark.shape[0] and img.shape[1] >= mark.shape[1]:#img is bigger than mark
-            mark = _replicate(mark, img.shape)
-        elif img.shape[0] < mark.shape[0] and img.shape[1] < mark.shape[1]:#img is smaller than mark    
-            mark = _cutImg(mark, img.shape)
-        elif img.shape[0] < mark.shape[0] and img.shape[1] > mark.shape[1]:#img is shorter than mark
-            if img.shape[0]*img.shape[1] >= mark.shape[0]*mark.shape[1]:
-                mark = _replicate(mark, img.shape)
-                else mark = _cutImg(img, size)
-            return img, green, mark
+        pass
+    elif img.shape[0]*img.shape[1] >= mark.shape[0]*mark.shape[1]:
+        mark = _replicate(mark, img.shape)
+    else：
+        mark = _cutImg(img, size)：
+    return img, green, mark
 #utils
 def _markProcess(img):
     return mfc.binarize(img)

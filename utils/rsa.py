7728aeef4340422ed36fa46db8bb2198e6aee51b
cctry <csycfl@gmail.com>
2016-12-20 20:41:25 +0800

    reduce dependency

--- utils/rsa.py
+++ utils/rsa.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*
 from Crypto import Random
-from Crypto.Hash import SHA
 from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
 from Crypto.Signature import PKCS1_v1_5 as Signature_pkcs1_v1_5
 from Crypto.PublicKey import RSA

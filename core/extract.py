1695f174a7bc07da49c453d8d05b84b5afd6ce0d
ipotato1 <534967057@qq.com>
2016-12-24 19:52:24 +0800

    amend

--- core/extract.py
+++ core/extract.py
@@ -4,20 +4,22 @@ from utils import rsa as RSA
 from preprocess import preprocess
 
 def extract(img,key):
-    testimg,img = ifc.getGChn(testimg)#flag
-    imgBlocks = ifc.splitImg(img)
+    if oriImg == 3：
+        oriImg,img = ifc.getGChn(oriImg)#flag
+    imgBlocks, size = ifc.splitImg(img)
     imgBlocks1 = map(lambda b: ifc.zeroLSB(b), imgBlocks)
     imgBlocks2 = ifc.extractLSB(imgBlocks)
-    imgBlocks2 = RSA.decrypt(key,imgBlocks2)
+    rsa = RSA.rsa()
+    rsa.generateKey(size[0]*size[1])#len
+    key = rsa.loadKey('public.pem')
+    imgBlocks3 = RSA.decrypt(key,imgBlocks2)
     blocks = list()
     for i in xrange(len(imgBlocks1)):
         imgBlock = imgBlocks1[i]
-        hashBits = mfc.hash(testImg.shape[1], testImg.shape[0], imgBlock1)
-        hashBits = mfc.cutHash(hashBits, imgBlock.shape[0]*imgBlock.shape[1])
-        bitmap = ifc.xor(imgBlocks2, hashBits)
-        decrypted_block = RSA.decrypt(key,msg)
-        block = ifc.insert2LSB(imgBlock, decrypted_block)
+        hashBits = mfc.hash(oriImg.shape[1], oriImg.shape[0], imgBlocks1)
+        hashBits = mfc.cutHash(hashBits, imgBlocks3.shape[0]*imgBlocks3.shape[1])
+        bitmap = ifc.xor(imgBlocks3, hashBits)
+        decrypted_block = RSA.decrypt(key,bitmap)
         blocks.append(block)
-    dst = ifc.assembleBlocks(blocks, (testImg.shape[0], testImg.shape[1]))
-    res = ifc.merge(dst,testImg)
-    return res
+    dst = ifc.assembleBlocks(blocks, (oriImg.shape[0], oriImg.shape[1]))
+    return dst

7f018d7eb4141ae78220c20fe0af2b7c490aa428
cctry <csycfl@gmail.com>
2016-12-24 12:38:14 +0800

    roughly finish

--- watermark.py
+++ watermark.py
@@ -1,20 +1,39 @@
 #coding:utf-8
 import cv2
 import sys
-from core import insert as is
+from core import insert as st
 from core import extract as xt
+from utils import imageFunc as ifc
+#main
 if len(sys.argv) <= 1 or len(sys.argv) > 6:
-    inputError()
-
+    argError()
 if sys.argv[1] == 'insert':
-    if len(sys.argv) == 4:
-        img = cv2.imread(sys.argv[2], CV_LOAD_IMAGE_UNCHANGED)
-        mark = cv2.imread(sys.argv[3], CV_LOAD_IMAGE_UNCHANGED)
-        if len(img.shape) == 1:
-            flag = 0
-        if len(img.shape) == 3:
-            flag = 1
-        _insert(img, mark, key)
+    img = cv2.imread(sys.argv[2], CV_LOAD_IMAGE_UNCHANGED)
+    mark = cv2.imread(sys.argv[3], CV_LOAD_IMAGE_UNCHANGED)
+    _insert(img, mark, key, flag)
+    exit()
+if sys.argv[1] == 'extract':
+    img = cv2.imread(sys.argv[2], CV_LOAD_IMAGE_UNCHANGED)
+    key = rsa.loadKey(sys.argv[3])
+    if len(sys.argv) == 6:#full arg with path
+        if sys.argv[4] != '-s':
+            argError()
+        path = sys.argv[5]        
+        dst = _extract(img, key)
+        cv2.imwrite(path+'extracted_'+sys.argv[2], dst)
+    if len(sys.argv) == 5:#save without path
+        if sys.argv[4] != '-s':
+            argError()       
+        dst = _extract(img, key)
+        cv2.imwrite('extracted_'+sys.argv[2], dst)
+    if len(sys.argv) == 4:#show the image
+        dst = _extract(img, key)
+        show(dst)
+        exit()
+else:
+    argError()
+
+#func
 def error(func):
     def newFunc():
         func()
@@ -22,8 +41,14 @@ def error(func):
     return newFunc
 
 @error
-def inputError():
+def argError():
     print 'Please input the correct agruments'
 
-def _insert(img, mark, key):
-    return is.insert(img, mark, key)
+def _insert(img, mark):
+    return st.insert(img, mark)
+
+def _extract(img, key):
+    return xt.extract(img,key)
+
+def show(img):
+    ifc.showImg(img)

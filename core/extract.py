201e53328322d80ed703dab4fa18d963253485d7
cctry <csycfl@gmail.com>
2017-01-03 09:54:43 +0800

    rewirte extract

--- core/extract.py
+++ core/extract.py
@@ -1,24 +1,21 @@
 from utils import imageFunc as ifc
 from utils import mathFunc as mfc
-from preprocess import preprocess
 import copy
 
 def extract(img):
-    testImg = copy.deepcopy(img)
-    if testImg.ndim == 3:#colored
-        img = ifc.getGChn(testImg)
+    oriImg = copy.deepcopy(img)
+    if oriImg.ndim == 3:
+        img = ifc.getGChn(img)
     imgBlocks = ifc.splitImg(img)
-    zeroedBlocks = map(lambda b: ifc.zeroLSB(b), imgBlocks)
-    extractedLSBs = map(lambda b: ifc.extractLSB(b), imgBlocks)
+    LSBs = map(lambda b: ifc.extractLSB(b), imgBlocks)
+    zeroedLSBs = map(lambda b: ifc.zeroLSB(b), imgBlocks)
     blocks = list()
     for i in xrange(len(imgBlocks)):
-        imgBlock = imgBlocks[i]
-        zeroedBlock = zeroedBlocks[i]
-        extractedLSB = extractedLSBs[i]
-        hashBits = mfc.hash(testImg.shape[1], testImg.shape[0], zeroedBlock)
-        hashBits = mfc.cutHash(hashBits, imgBlock.shape[0]*imgBlock.shape[1])#64
-        dstBlock = ifc.xor(extractedLSB, hashBits)
+        imgBlock = zeroedLSBs[i]
+        LSB = LSBs[i]
+        hashBits = mfc.hash(imgBlock.shape[1], imgBlock.shape[0], imgBlock)
+        hashBits = mfc.cutHash(hashBits, imgBlock.shape[0] * imgBlock.shape[1])
+        dstBlock = ifc.xor(LSB, hashBits)
         blocks.append(dstBlock)
-    dst = ifc.assembleBlocks(blocks, (testImg.shape[0], testImg.shape[1]))
-    dst *= 255
-    return dst
+    dst = ifc.assembleBlocks(blocks, (oriImg.shape[0], oriImg.shape[1]))
+    return dst*255
